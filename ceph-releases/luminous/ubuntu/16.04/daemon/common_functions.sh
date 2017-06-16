8288b5bf34e9e0fb6d069995c96ace64f6d04b9f
Sébastien Han <seb@redhat.com>
2017-06-16 16:14:43 +0200

    daemon: include demo into daemon
    First attempt to include the demo container image inside the daemon
    image. 'demo' will then become a new scenario which will run a container
    with all the Ceph daemons.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- ceph-releases/luminous/ubuntu/16.04/daemon/common_functions.sh
+++ ceph-releases/luminous/ubuntu/16.04/daemon/common_functions.sh
@@ -307,3 +307,19 @@ function is_pid_ns {
   [[ $(ps --no-header x | wc -l) -gt 3 ]]
 }
 
+# This function is only used when CEPH_DAEMON=demo
+# For a 'demo' container, we must ensure there is no Ceph files
+function detect_ceph_files {
+  if [ -d /var/lib/ceph ] || [ -d /etc/ceph ]; then
+    return 0
+  fi
+  if [ -f /etc/ceph/I_AM_A_DEMO ] && [ -f /var/lib/ceph/I_AM_A_DEMO ]; then
+    return 0
+  fi
+  if [[  "$(find /var/lib/ceph/ -mindepth 3 -maxdepth 3 -type f | wc -l)" != 0 ]] || [[ -z "$(find /etc/ceph -prune -empty)" ]]; then
+    log "I can see existing Ceph files, please remove them!"
+    log "Remove the content of /var/lib/ceph/ and /etc/ceph/"
+    log "Doing nothing."
+    exit 1
+  fi
+}
